a8b3617c9f22cf36171d5faa32e011221f2fc813
Phoenix-0921 <jyu85921@gmail.com>
2023-07-30 00:48:14 +0800

    @CrossOrigin(value = "*") 加上跨域請求

--- chat/src/main/java/com/example/chat/controller/CharRoomController.java
+++ chat/src/main/java/com/example/chat/controller/CharRoomController.java
@@ -3,10 +3,12 @@ package com.example.chat.controller;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Controller
 public class CharRoomController {
     @MessageMapping("/messageControl")
+    @CrossOrigin(value = "*")
     @SendTo("/topic/getResponse")
     public String handleChatMessage(String message) throws InterruptedException {
         // 模擬處理訊息的時間（這裡設置為3秒）
--- chat/src/main/java/com/example/chat/controller/RandomChatRoomController.java
+++ chat/src/main/java/com/example/chat/controller/RandomChatRoomController.java
@@ -3,8 +3,11 @@ package com.example.chat.controller;
 import com.example.chat.Service.RandomChatRoomService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+
 
 
 @RestController
@@ -13,6 +16,7 @@ public class RandomChatRoomController {
     private RandomChatRoomService randomChatRoomService;
 
     @PostMapping("/chat/match")
+    @CrossOrigin(value = "*") //允許跨域請求
     public ResponseEntity<String> matchUsers() {
         randomChatRoomService.matchUsers();
         return ResponseEntity.ok("已發起配對請求，請等待匹配結果。");
